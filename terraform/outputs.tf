287b48b38d441efa0be786e921ddf83040db0ce0
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 17:38:12 +0530

    Add output variables for Azure resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,30 @@
 
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "vnet_name" {
+  description = "Name of the Virtual Network"
+  value       = azurerm_virtual_network.main.name
+}
+
+output "vnet_id" {
+  description = "Resource ID of the Virtual Network"
+  value       = azurerm_virtual_network.main.id
+}
+
+output "subnet_id" {
+  description = "Resource ID of the application subnet"
+  value       = azurerm_subnet.app.id
+}
+
+output "nsg_id" {
+  description = "Resource ID of the Network Security Group"
+  value       = azurerm_network_security_group.main.id
+}
+
+output "storage_account_name" {
+  description = "Name of the Storage Account"
+  value       = azurerm_storage_account.main.name
+}

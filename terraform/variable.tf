f4482513169cffd33b9bf5653e07e72c0663041a
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 17:37:12 +0530

    Define Terraform variables for Azure resources
    Added variables for resource group, location, environment, VNet address space, and subnet address prefix.

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,1 +1,28 @@
 
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "East US"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, stage, prod)"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the Virtual Network"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_address_prefix" {
+  description = "Address prefix for the application subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
